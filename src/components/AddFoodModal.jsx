f241db540844232fe5833849ff4683d654a46fad
anirudhct <anirudh@connectia.in>
2024-05-07 14:51:30 +0530

    changes addfood

--- src/components/AddFoodModal.jsx
+++ src/components/AddFoodModal.jsx
@@ -1,147 +1,254 @@
-import React, { useEffect, useState } from 'react'
-import Input from './Input'
-import Select from './Select'
-import Button from './Button'
-import axios from 'axios'
-import SearchableSelect from './SearchableSelect'
-import { UseProgramContext } from '../context/Program'
-import { UseIngredientsContext } from '../context/Ingredients'
-import UploadComponent from './UploadComponent'
+import React, { useEffect, useState } from "react";
+import Input from "./Input";
+import Select from "./Select";
+import Button from "./Button";
+import axios from "axios";
+import SearchableSelect from "./SearchableSelect";
+import { UseProgramContext } from "../context/Program";
+import { UseIngredientsContext } from "../context/Ingredients";
+import UploadComponent from "./UploadComponent";
 
 const AddFoodModal = ({ showModal, setShowModal }) => {
-  const { programOptions } = UseProgramContext()
-  const { ingredientsOptions } = UseIngredientsContext()
+  const { programOptions } = UseProgramContext();
+  const { ingredientsOptions } = UseIngredientsContext();
   const initialState = {
-    "name": "",
-    "badge": "",
-    "protien": "",
-    "fat": "",
-    "carbs": "",
-    "calories": "",
-    "category": "snack",
-    "foodcode": ""
-  }
+    name: "",
+    badge: "",
+    protien: "",
+    fat: "",
+    carbs: "",
+    calories: "",
+    category: "snack",
+    foodcode: "",
+  };
   const [formstate, setFormState] = useState(initialState);
   const [foodIngredients, setFoodIngredients] = useState([]);
-  const [image, setImage] = useState("")
+  const [image, setImage] = useState("");
 
   const handleChange = (e) => {
     setFormState({
       ...formstate,
-      [e.target.id]: e.target.value
-    })
-  }
+      [e.target.id]: e.target.value,
+    });
+  };
 
   useEffect(() => {
     if (showModal?.update && showModal.data) {
-      setFormState(showModal.data)
-      setFoodIngredients(showModal.data.ingredients)
-      setImage(showModal?.data?.image)
+      setFormState(showModal.data);
+      setFoodIngredients(showModal.data.ingredients);
+      setImage(showModal?.data?.image);
     }
-  }, [showModal])
-
+  }, [showModal]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (showModal?.update) {
       axios(`${process.env.REACT_APP_BASE_URL}/food/${showModal?.data?._id}`, {
         method: "PATCH",
-        data: { ...formstate, ingredients: foodIngredients?.map((data) => {
-          return data?._id
-        }), image },
+        data: {
+          ...formstate,
+          ingredients: foodIngredients?.map((data) => {
+            return data?._id;
+          }),
+          image,
+        },
         headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`
-        }
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
       })
         .then((res) => {
-          alert(res.data.message)
-          setShowModal({show: false, update: false, data: undefined})
+          alert(res.data.message);
+          setShowModal({ show: false, update: false, data: undefined });
         })
         .catch((err) => {
-          alert(err.message)
-        })
+          alert(err.message);
+        });
     } else {
       axios(`${process.env.REACT_APP_BASE_URL}/food/create`, {
         method: "POST",
-        data: { ...formstate, ingredients: foodIngredients?.map((data) => {
-          return data?._id
-        }), image },
+        data: {
+          ...formstate,
+          ingredients: foodIngredients?.map((data) => {
+            return data?._id;
+          }),
+          image,
+        },
         headers: {
-          Authorization: `Bearer ${localStorage.getItem("token")}`
-        }
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
       })
         .then((res) => {
-          alert(res.data.message)
-          setShowModal({show: false, update: false, data: undefined})
+          alert(res.data.message);
+          setShowModal({ show: false, update: false, data: undefined });
         })
         .catch((err) => {
-          alert(err.message)
-        })
+          alert(err.message);
+        });
     }
-  }
+  };
 
   return (
     <>
-      <div id="defaultModal" tabindex="-1" aria-hidden="true" className={showModal?.show ? "fixed top-0 left-0 right-0 z-50  w-full p-4 overflow-x-hidden bg-black/50 overflow-y-auto h-screen opacity-100 duration-500 flex items-center justify-center" : " flex items-center justify-centerfixed top-0 left-0 right-0 z-50  w-full p-4 overflow-x-hidden bg-black/50 overflow-y-auto h-screen opacity-0 pointer-events-none duration-500"}>
+      <div
+        id="defaultModal"
+        tabindex="-1"
+        aria-hidden="true"
+        className={
+          showModal?.show
+            ? "fixed top-0 left-0 right-0 z-50  w-full p-4 overflow-x-hidden bg-black/50 overflow-y-auto h-screen opacity-100 duration-500 flex items-center justify-center"
+            : " flex items-center justify-centerfixed top-0 left-0 right-0 z-50  w-full p-4 overflow-x-hidden bg-black/50 overflow-y-auto h-screen opacity-0 pointer-events-none duration-500"
+        }
+      >
         <div className="relative w-[70%] max-h-full overflow-y-scroll m-auto">
-
-          <form onSubmit={handleSubmit} className="relative rounded-lg shadow bg-darkGray">
-
+          <form
+            onSubmit={handleSubmit}
+            className="relative rounded-lg shadow bg-darkGray"
+          >
             <div className="flex items-start justify-between p-4 border-b rounded-t border-textGray ">
               <h3 className="text-xl font-semibold text-white">
                 {showModal?.update ? "Update Food" : "Add Food"}
               </h3>
-              <button onClick={() => { setShowModal({ show: false, update: false, data: undefined }) }} type="button" className="text-white bg-transparent hover:bg-gray-600 duration-200 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center ">
-                <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+              <button
+                onClick={() => {
+                  setShowModal({ show: false, update: false, data: undefined });
+                }}
+                type="button"
+                className="text-white bg-transparent hover:bg-gray-600 duration-200 rounded-lg text-sm w-8 h-8 ml-auto inline-flex justify-center items-center "
+              >
+                <svg
+                  className="w-3 h-3"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 14 14"
+                >
+                  <path
+                    stroke="currentColor"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+                  />
                 </svg>
               </button>
             </div>
 
             <div className="p-6 space-y-4">
               <UploadComponent setImage={setImage} image={image} />
-              <div className='grid grid-cols-2 gap-x-4'>
-                <Input value={formstate.name} id={"name"} onChange={handleChange} label={"Name"} type={"text"} placeholder={"Name of Food"} />
-                <Input value={formstate.badge} id={"badge"} onChange={handleChange} label={"Badge"} type={"text"} placeholder={"Badge"} />
+              <div className="grid grid-cols-2 gap-x-4">
+                <Input
+                  value={formstate.name}
+                  id={"name"}
+                  onChange={handleChange}
+                  label={"Name"}
+                  type={"text"}
+                  placeholder={"Name of Food"}
+                />
+                <Input
+                  value={formstate.badge}
+                  id={"badge"}
+                  onChange={handleChange}
+                  label={"Badge"}
+                  type={"text"}
+                  placeholder={"Badge"}
+                />
               </div>
-              <Input value={formstate.foodcode} id={"foodcode"} onChange={handleChange} label={"Food Code"} type={"text"} placeholder={"Food Code"} />
-              <SearchableSelect label={"Ingredients"} onChange={(e) => {
-                setFoodIngredients(e)
-              }} value={foodIngredients?.map((data) => {
-                return { ...data, label: data?.title, value: data._id }
-              })} id={"ingredients"} isMulti={true} placeholder={"Select the Ingredients"} options={ingredientsOptions} />
+              <Input
+                value={formstate.foodcode}
+                id={"foodcode"}
+                onChange={handleChange}
+                label={"Food Code"}
+                type={"text"}
+                placeholder={"Food Code"}
+              />
+              <SearchableSelect
+                label={"Ingredients"}
+                onChange={(e) => {
+                  setFoodIngredients(e);
+                }}
+                value={foodIngredients?.map((data) => {
+                  return { ...data, label: data?.title, value: data._id };
+                })}
+                id={"ingredients"}
+                isMulti={true}
+                placeholder={"Select the Ingredients"}
+                options={ingredientsOptions}
+              />
               {/* <SearchableSelect label={"Allergic Items"} id={"allergy"} isMulti={true} placeholder={"Select the Allergic Items"} options={ingredientsOptions} /> */}
-              <Select value={formstate.protien} onChange={handleChange} id={"category"} label={"Category"} options={[
-                {
-                  label: "Breakfast",
-                  value: "breakfast"
-                },
-                {
-                  label: "Main Dish",
-                  value: "main-dish"
-                },
-                {
-                  label: "Snack",
-                  value: "snack"
-                },
-              ]} />
-              <div className='grid grid-cols-4 gap-x-4'>
-                <Input value={formstate.protien} onChange={handleChange} id={"protien"} label={"Protien"} type={"text"} placeholder={"Protien"} />
-                <Input value={formstate.fat} onChange={handleChange} id={"fat"} label={"Fat"} type={"text"} placeholder={"Fat"} />
-                <Input value={formstate.carbs} onChange={handleChange} id={"carbs"} label={"Carbs"} type={"text"} placeholder={"Carbs"} />
-                <Input value={formstate.calories} onChange={handleChange} id={"calories"} label={"Calories"} type={"text"} placeholder={"Calories"} />
+              <Select
+                value={formstate.category}
+                onChange={handleChange}
+                id={"category"}
+                label={"Category"}
+                options={[
+                  {
+                    label: "Breakfast",
+                    value: "breakfast",
+                  },
+                  {
+                    label: "Main Dish",
+                    value: "main-dish",
+                  },
+                  {
+                    label: "Snack",
+                    value: "snack",
+                  },
+                ]}
+              />
+              <div className="grid grid-cols-4 gap-x-4">
+                <Input
+                  value={formstate.protien}
+                  onChange={handleChange}
+                  id={"protien"}
+                  label={"Protien"}
+                  type={"text"}
+                  placeholder={"Protien"}
+                />
+                <Input
+                  value={formstate.fat}
+                  onChange={handleChange}
+                  id={"fat"}
+                  label={"Fat"}
+                  type={"text"}
+                  placeholder={"Fat"}
+                />
+                <Input
+                  value={formstate.carbs}
+                  onChange={handleChange}
+                  id={"carbs"}
+                  label={"Carbs"}
+                  type={"text"}
+                  placeholder={"Carbs"}
+                />
+                <Input
+                  value={formstate.calories}
+                  onChange={handleChange}
+                  id={"calories"}
+                  label={"Calories"}
+                  type={"text"}
+                  placeholder={"Calories"}
+                />
               </div>
-              <Input value={formstate.description} id={"description"} onChange={handleChange} label={"Description"} type={"text"} placeholder={"Description"} />
+              <Input
+                value={formstate.description}
+                id={"description"}
+                onChange={handleChange}
+                label={"Description"}
+                type={"text"}
+                placeholder={"Description"}
+              />
             </div>
             <div className="flex items-center justify-center pb-6  rounded-b ">
-              <Button text={showModal?.update ? "Update Food" : "Add Food"} className={"w-1/6"} />
+              <Button
+                text={showModal?.update ? "Update Food" : "Add Food"}
+                className={"w-1/6"}
+              />
             </div>
           </form>
         </div>
       </div>
-
     </>
-  )
-}
+  );
+};
 
-export default AddFoodModal
+export default AddFoodModal;
--- src/pages/ListFood.jsx
+++ src/pages/ListFood.jsx
@@ -32,7 +32,7 @@ const ListFood = () => {
               <Button buttonClassName={"w-auto px-3 py-1"} text={"Print"} />
             </div>
 
-            <div className="w-full text-white h-[88vh] overflow-auto rounded-lg">
+            <div className="w-full text-white h-[78vh] overflow-auto rounded-lg pb-5">
               <table className="w-full text-left bg-darkGray ">
                 <thead className='sticky top-0 overflow-hidden '>
                   <tr className='bg-mediumGray rounded-t-lg'>
@@ -44,7 +44,7 @@ const ListFood = () => {
                   </tr>
                 </thead>
 
-                <tbody className='text-sm'>
+                <tbody className='text-sm '>
                   {
                     food?.map((data, index) => {
                       return <tr key={index} className="border-b border-mediumGray">
